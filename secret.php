a53f5d4606cd453d64e1830afb93d97c57579344
Boyan Yurukov <yurukov@gmail.com>
2012-01-14 18:50:53 +0100

    flood stopper

--- secret.php
+++ secret.php
@@ -2,7 +2,7 @@
 
 $secret="Troll stopper :)";
 
-if (!isset($_GET[$secret])) {
+if (!isset($_GET[$secret]) && !isset($argv)) {
 	echo "<img src='http://upload.wikimedia.org/wikipedia/en/thumb/7/78/Trollface.svg/365px-Trollface.svg.png'/>";
 	exit;
 }
